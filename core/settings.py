ccd55b56e8487a724b018e3a5f6af6e081941d83
nam nguyen thanh <bffkudo1412@gmail.com>
2023-09-04 13:13:07 +0700

    ok

--- core/settings.py
+++ core/settings.py
@@ -33,8 +33,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # DEBUG = True
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ['nam-thanh-1307.onrender.com']
+
 
 
 # Application definition
